e1efa9f5986afdbe811ba256cff96607a2bd90fe
Mark Leybourne <mark.leybourne@avecto.com>
2017-09-08 08:08:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wercker_test_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
